7b152d6b88bbd7e2f50376b95bb9c4434261be2a
Lucas Gabriel <lucasgabriel.shig@hotmail.com>
2024-05-10 18:52:00 -0300

    feat: second section (mobile & desk)

--- src/components/Sections/SecondSection/index.jsx
+++ src/components/Sections/SecondSection/index.jsx
@@ -1,15 +1,15 @@
+import PaperTexture from "../../../assets/paper.jpg";
+import style from "./style.module.scss";
+
 export const SecondSection = () => {
     return(
-        <section>
-            <img src="" alt="paper texture" />
-            <img src="" alt="number zero" />
-            <div>
-                <div>
-                    <h2 className="title two black">Invista sem se preocupar com taxas</h2>
-                    <p className="text black">
-                    A CM Capital tem corretagem zero para tudo! Opere com ações, contratos futuros, minicontratos, opções, commodities, Fundos Imobiliários, BDR, ETF, S&P Futuro e muito mais, sem se preocupar com taxas!
-                    </p>
-                </div>
+        <section className={style.sectionBox}>
+            <img src={PaperTexture} alt="paper texture" />
+            <div className={style.textBox}>           
+                <h2 className="title two black">Invista sem se preocupar com taxas</h2>
+                <p className="text black">
+                A CM Capital tem corretagem zero para tudo! Opere com ações, contratos futuros, minicontratos, opções, commodities, Fundos Imobiliários, BDR, ETF, S&P Futuro e muito mais, sem se preocupar com taxas!
+                </p>
                 <button className="button large blue">
                     Abra sua conta
                 </button>
